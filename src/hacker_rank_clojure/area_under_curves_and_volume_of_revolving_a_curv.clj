28e56d2c17122d2273098ef1cf9f5cdb040f2f50
Shintaro Matsushima <shintaromatsushima@gmail.com>
2017-12-06 21:18:40 +0900

    Implemented volume of revolving a curve

--- src/hacker_rank_clojure/area_under_curves_and_volume_of_revolving_a_curv.clj
+++ src/hacker_rank_clojure/area_under_curves_and_volume_of_revolving_a_curv.clj
@@ -1,4 +1,5 @@
-(ns hacker-rank-clojure.area-under-curves-and-volume-of-revolving-a-curv)
+(ns hacker-rank-clojure.area-under-curves-and-volume-of-revolving-a-curv
+  (:require [clojure.string :as str]))
 
 
 (def n 1000)
@@ -6,19 +7,21 @@
 
 (defn area
   [a b x]
-  (reduce + (map #(* %1 (Math/pow x %2)) a b)))
+  (let [x (/ (Math/ceil (* n x)) n)]
+    (reduce + (map #(* %1 (Math/pow x %2)) a b))))
 
 (defn volume
   [a b x]
-
-  )
+  (let [r (area a b x)]
+    (* Math/PI r r)))
 
 (defn summation
   "https://www.hackerrank.com/challenges/area-under-curves-and-volume-of-revolving-a-curv/problem"
   [f l r a b]
   (/ (reduce + (map #(f a b %) (range l r invert-n))) n))
 
-(range 1 4 (double (/ 1 n)))
+(defn parse-line
+  [l]
+  (map #(Integer/parseInt %) (clojure.string/split l #" ")))
+
 ;; entry point
-(summation area 1 4 [1 2 3 4 5] [6 7 8 9 10])
-(summation volume 1 4 [1 2 3 4 5] [6 7 8 9 10])
