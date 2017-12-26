5340b33a6a75fcc5fc0a17973b2975963dc7c278
Shintaro Matsushima <shintaromatsushima@gmail.com>
2017-12-26 22:39:44 +0900

    Solved "String Compression"

--- src/hacker_rank_clojure/string_compression.clj
+++ src/hacker_rank_clojure/string_compression.clj
@@ -2,18 +2,9 @@
 
 (defn string-compression
   "https://www.hackerrank.com/challenges/string-compression/problem"
-  [t r i]
-  (cond
-    (empty? r)
-    (if (> i 1) (str t i) t)
-
-    (not= (last t) (first r))
-    (if (> i 1)
-      (recur (str t i (first r)) (rest r) 1)
-      (recur (str t (first r)) (rest r) 1))
-
-    :else (recur t (rest r) (inc i))))
+  [s]
+  (clojure.string/replace s #"(\w)\1+" #(str (last %) (count (first %)))))
 
 ;; entry point
 (let [s (read-line)]
-  (println (string-compression "" (vec s) 1)))
+  (println (string-compression s)))
