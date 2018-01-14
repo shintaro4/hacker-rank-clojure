30ffc26afede9fe3c82280fb3e5f4aced170077a
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-01-14 18:02:40 +0900

    Timed out solution

--- src/hacker_rank_clojure/functional_programming_sums_of_powers.clj
+++ src/hacker_rank_clojure/functional_programming_sums_of_powers.clj
@@ -1,6 +1,10 @@
 (ns hacker-rank-clojure.functional-programming-sums-of-powers)
 
 
+(defn sum-of-powers
+  [n v]
+  (reduce + (map #(Math/pow % n) v)))
+
 (defn combinations
   [n items]
   (cond
@@ -11,12 +15,19 @@
                    (combinations (dec n) (rest items)))
                   (combinations n (rest items)))))
 
+(defn all-combinations
+  [x n]
+  (let [v (range 1 (Math/floor (inc (Math/pow x (/ 1 n)))))]
+    (mapcat #(combinations % v) v)))
+
 (defn functional-programming-sums-of-powers
-  []
+  [x n]
   "https://www.hackerrank.com/challenges/functional-programming-the-sums-of-powers/problem"
-  )
+  (->> (all-combinations x n)
+       (filter #(= x (int (sum-of-powers n %))))
+       (count)))
 
 ;; entry point
-(let [x (Integer/parseInt) (read-line)
-      n (Integer/parseInt) (read-line)]
+(let [x (Integer/parseInt (read-line))
+      n (Integer/parseInt (read-line))]
   (println (functional-programming-sums-of-powers x n)))
