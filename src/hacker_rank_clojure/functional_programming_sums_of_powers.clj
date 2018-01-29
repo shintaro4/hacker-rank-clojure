db0d81cd650924e4da4f1cc616d65b868f709ac8
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-01-29 22:11:03 +0900

    Solved "The Sums of Powers"

--- src/hacker_rank_clojure/functional_programming_sums_of_powers.clj
+++ src/hacker_rank_clojure/functional_programming_sums_of_powers.clj
@@ -1,31 +1,29 @@
 (ns hacker-rank-clojure.functional-programming-sums-of-powers)
 
 
-(defn sum-of-powers
-  [n v]
-  (reduce + (map #(Math/pow % n) v)))
-
-(defn combinations
-  [n items]
-  (cond
-    (= n 0) '(())
-    (empty? items) '()
-    :else (concat (map
-                   #(cons (first items) %)
-                   (combinations (dec n) (rest items)))
-                  (combinations n (rest items)))))
-
-(defn all-combinations
+(defn candidates
   [x n]
-  (let [v (range 1 (Math/floor (inc (Math/pow x (/ 1 n)))))]
-    (mapcat #(combinations % v) v)))
+  (for [k (range 1 x)
+        :let [y (int (Math/pow k n))]
+        :while (<= y x)]
+    y))
+
+(defn find-ways
+  [n xs ys]
+  (cond
+    (< n 0) []
+    (= n 0) [ys]
+    :else (mapcat
+           (fn [k]
+             (find-ways (- n k)
+                        (filter #(> % k) xs)
+                        (cons k ys)))
+           (take-while #(<= % n) xs))))
 
 (defn functional-programming-sums-of-powers
   [x n]
   "https://www.hackerrank.com/challenges/functional-programming-the-sums-of-powers/problem"
-  (->> (all-combinations x n)
-       (filter #(= x (int (sum-of-powers n %))))
-       (count)))
+  (count (find-ways x (candidates x n) [])))
 
 ;; entry point
 (let [x (Integer/parseInt (read-line))
