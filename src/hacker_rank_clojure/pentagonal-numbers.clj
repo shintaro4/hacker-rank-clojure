a79d1836ca22a9ad437bc3fda22a2a8553518875
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-02-11 22:17:26 +0900

    Solved "Pentagonal Numbers"

--- src/hacker_rank_clojure/pentagonal-numbers.clj
+++ src/hacker_rank_clojure/pentagonal-numbers.clj
@@ -1,21 +1,24 @@
 (ns hacker-rank-clojure.pentagonal-numbers)
 
 
-(def N 100000)
+(def N (+ 100000 1))
+(def DP (make-array Long/TYPE N))
 
 (defn pentagonal-numbers
   []
   "https://www.hackerrank.com/challenges/pentagonal-numbers/problem"
-  (map #)
-  (loop [i 1
-         dp [1]]
-    (if (= i 1000) dp
-        (recur (inc i) (conj dp (+ (last dp) (- (* (inc i) 3) 2)))))))
+  (aset DP 1 1)  ; set bottom
+  (loop [i 2]
+    (when (< i N)
+      (aset DP i
+            (+ (aget DP (dec i)) (- (* i 3) 2)))
+      (recur (inc i)))))
 
 ;; entry point
-(let [dp (pentagonal-numbers)]
+(pentagonal-numbers)
+(let [dp (into [] DP)]
   (loop [i 0
          t (Integer/parseInt (read-line))]
     (when (< i t)
-      (println (get dp (dec (Integer/parseInt (read-line)))))
+      (println (get dp (Integer/parseInt (read-line))))
       (recur (inc i) t))))
