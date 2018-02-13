7b53d4e74ef9707b73e7f26310de45327980a3d1
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-02-14 00:09:42 +0900

    [WIP] "Different Ways"

--- src/hacker_rank_clojure/different_ways_fp.clj
+++ src/hacker_rank_clojure/different_ways_fp.clj
@@ -1,24 +1,29 @@
 (ns hacker-rank-clojure.different-ways-fp)
 
-
 (def N (+ 10000 1))
-(def DP (atom [0N 1N]))
+(def DP (atom [[][1N 1N]]))
 (def P 100000007N)
 
 (defn different-ways
   []
   "https://www.hackerrank.com/challenges/different-ways-fp/problem"
-  (loop [i 2]
-    (when (< i N)
-      (swap! DP conj (+ (nth @DP (- i 1))
-                        (nth @DP (- i 2))))
-      (recur (inc i)))))
+  (doseq [i (range 2 N)]
+    (let [p (last @DP)
+          c (into [] (map #(if (or (= % 0) (= % i)) 1N
+                               (+ (nth p (dec %)) (nth p %)))
+                          (range (inc i))))]
+      (swap! DP conj c))))
 
 ;; entry point
+(different-ways)
 (loop [i 0
        t (Integer/parseInt (read-line))]
   (when (< i t)
-    (let [n (nth @DP (Integer/parseInt (read-line)))
-          m (rem n P)]
+    (let [l (clojure.string/split (read-line) #" ")
+          n (Integer/parseInt (first l))
+          k (Integer/parseInt (second l))
+          c (nth @DP n)
+          v (nth c k)
+          m (rem v P)]
       (println (format "%d" (biginteger m))))
     (recur (inc i) t)))
