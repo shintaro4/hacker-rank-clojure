da3c31e1fce81495e3f5e3e0b734563afbd3091f
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-02-18 16:00:11 +0900

    [WIP]

--- src/hacker_rank_clojure/different_ways_fp.clj
+++ src/hacker_rank_clojure/different_ways_fp.clj
@@ -1,24 +1,25 @@
 (ns hacker-rank-clojure.different-ways-fp)
 
 
-(def N (+ 10000 1))
-(def DP (atom [1N 1N]))
+(def N 10000)
+(def S (- (/ (* (+ N 1) (+ N 2)) 2) 1))
+(def DP (atom (vec (repeat S 1N))))
 (def P 100000007N)
 
 (defn different-ways
   []
   "https://www.hackerrank.com/challenges/different-ways-fp/problem"
-  (doseq [i (range 2 N)]
+  (doseq [i (range 2 (inc N))]
     (let [p (- (/ (* i (inc i)) 2) 1)]
-      (doseq [j (range (inc i))]
+      (doseq [j (range 1 i)]
         (let [k (+ p j)
               q (- (/ (* (dec i) i) 2) 1)
-              c (if (or (= j 0) (= j i)) 1N
-                    (+ (nth @DP (+ q (dec j))) (nth @DP (+ q j))))]
+              c (+ (nth @DP (+ q (dec j))) (nth @DP (+ q j)))]
           (swap! DP assoc k c))))))
 
 ;; entry point
 (different-ways)
+
 (loop [i 0
        t (Integer/parseInt (read-line))]
   (when (< i t)
