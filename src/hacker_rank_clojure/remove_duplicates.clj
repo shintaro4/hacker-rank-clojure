f271f7c100ed08640fb6561f8a5be9d41ebda529
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-02-22 21:55:51 +0900

    [WIP] "Remove Duplicates"

---
+++ src/hacker_rank_clojure/remove_duplicates.clj
@@ -0,0 +1,10 @@
+(ns hacker-rank-clojure.remove-duplicates)
+
+(defn remove-duplicates
+  "https://www.hackerrank.com/challenges/remove-duplicates/problem"
+  [s]
+  )
+
+;; entry point
+(let [s (read-line)]
+  (println (remove-duplicates s)))
---
+++ test/hacker_rank_clojure/remove_duplicates_test.clj
@@ -0,0 +1,8 @@
+(ns hacker-rank-clojure.remove-duplicates-test
+  (:require [clojure.test :refer :all]
+            [hacker-rank-clojure.remove-duplicates :refer :all]))
+
+(deftest test-remove-duplicates
+  (testing "sample I/O."
+    (is (= "abc" (remove-duplicates "aabc"))
+        (= "cba" (remove-duplicates "ccbabacc")))))
