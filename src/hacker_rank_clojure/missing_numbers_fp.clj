437c080ecbbd134ccc6d02f162da6ffa5a810e67
Shintaro Matsushima <shintaromatsushima@gmail.com>
2018-02-27 21:47:41 +0900

    Solved "Missing Numbers (FP)"

--- src/hacker_rank_clojure/missing_numbers_fp.clj
+++ src/hacker_rank_clojure/missing_numbers_fp.clj
@@ -4,24 +4,21 @@
 (defn missing-numbers-fp
   "https://www.hackerrank.com/challenges/missing-numbers-fp/problem"
   [a b]
-  (loop [ta (vec a)
-         tb (vec b)
+  (loop [ta (sort a)
+         tb (sort b)
          m #{}]
     (if (empty? tb) m
         (let [x (first tb)
-              i (.indexOf ta x)]
-          (if (= i -1)
-            (recur ta (rest tb) (conj m x))
-            (recur (into [] (concat (subvec ta 0 i)
-                                    (subvec ta (inc i))))
-                   (rest tb)
-                   m))))))
+              y (first ta)]
+          (if (= x y)
+            (recur (rest ta) (rest tb) m)
+            (recur ta (rest tb) (conj m x)))))))
 
 ;; entry point
 (let [_n (read-line)
-      a (map #(Integer/parseInt %)(clojure.string/split (read-line) #" "))
+      a (mapv #(Integer/parseInt %)(clojure.string/split (read-line) #" "))
       _m (read-line)
-      b (map #(Integer/parseInt %)(clojure.string/split (read-line) #" "))
+      b (mapv #(Integer/parseInt %)(clojure.string/split (read-line) #" "))
       m (missing-numbers-fp a b)
       s (clojure.string/join " " (apply sorted-set m))]
   (println s))
